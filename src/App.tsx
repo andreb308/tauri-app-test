382ec349a09c94701e3f2e3185e0bee4f824fd2e
André Barros <107200960+andreb308@users.noreply.github.com>
2024-03-04 19:47:36 -0300

    feat: fetching test

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,42 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, _] = useState("");
+type CatAPIResponse = {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}[];
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+function App() {
+  // const [greetMsg, setGreetMsg] = useState("");
+  const [imageURL, setImageURL] = useState(
+    "https://cdn2.thecatapi.com/images/4q6.gif"
+  );
+
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  //   setGreetMsg(await invoke("greet", { name }));
+  // }
+
+  const fetchCat = async () => {
+    const api_url = "https://api.thecatapi.com/v1/images/search";
+    const cat: CatAPIResponse = await fetch(
+      "https://api.thecatapi.com/v1/images/search"
+    ).then((res) => res.json());
+    setImageURL(cat[0].url);
+    console.log(cat[0].url);
+  };
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <img
+        style={{ width: 750, height: 500, objectFit: "contain" }}
+        src={imageURL}
+        alt=""
+      />
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => console.log(e.target.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <button onClick={() => fetchCat()}>Fetch New Cat</button>
     </div>
   );
 }
