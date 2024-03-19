0e75f96acb8223c6450f7699dfa9534b87d10bff
André Barros <107200960+andreb308@users.noreply.github.com>
2024-03-19 11:06:06 -0300

    feat: (cat)rousel
    * api fetch now stores 10 images and renders them in a carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+let variavel: number | string = "teste";
 
 type CatAPIResponse = {
   id: string;
@@ -15,30 +29,58 @@ function App() {
   const [imageURL, setImageURL] = useState(
     "https://cdn2.thecatapi.com/images/4q6.gif"
   );
-
+  const [data, setData] = useState<CatAPIResponse>([]);
   // async function greet() {
   //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   //   setGreetMsg(await invoke("greet", { name }));
   // }
 
   const fetchCat = async () => {
-    const api_url = "https://api.thecatapi.com/v1/images/search";
-    const cat: CatAPIResponse = await fetch(
-      "https://api.thecatapi.com/v1/images/search"
+    setData([]);
+    const cats: CatAPIResponse = await fetch(
+      "https://api.thecatapi.com/v1/images/search?limit=10"
     ).then((res) => res.json());
-    setImageURL(cat[0].url);
-    console.log(cat[0].url);
+    setData(cats);
+    setImageURL(cats[0].url);
+    console.log(cats[0].url);
   };
 
   return (
-    <div className="container">
-      <img
-        style={{ width: 750, height: 500, objectFit: "contain" }}
-        src={imageURL}
-        alt=""
-      />
+    <div className=" items-center justify-center container">
+      <Carousel className="w-full max-w-3xl ">
+        <CarouselContent>
+          {data &&
+            data.map((pet, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex items-center justify-center p-6">
+                      <img
+                        style={{
+                          width: 1500,
+                          height: 500,
+                          objectFit: "contain",
+                        }}
+                        src={pet.url}
+                        alt=""
+                      />
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
 
-      <button onClick={() => fetchCat()}>Fetch New Cat</button>
+      <Button
+        style={{ width: 150 }}
+        variant={"destructive"}
+        onClick={() => fetchCat()}
+      >
+        Fetch New Cats
+      </Button>
     </div>
   );
 }
